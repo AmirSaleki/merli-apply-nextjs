f3c0267f5c48b27924e703ff6aeb565a0d23ee37
AmirSaleki <amir.saleki8@gmail.com>
2022-02-10 21:51:28 +0100

    first commit

---
+++ API_KEY.js
@@ -0,0 +1,7 @@
+const emailAPI = {
+  user: "user_n5002173eUYll5nRzHxmv",
+  service: "service_qavlhho",
+  template: "template_n8g7sy5",
+};
+
+export default emailAPI;
---
+++ components/Navbar/Navbar.component.jsx
@@ -0,0 +1,59 @@
+import logo from "../../logo.svg";
+import css from "./Navbar.module.css";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [hamClick, setHamClick] = useState(false);
+  const hamMenuClickHandler = () => {
+    setHamClick(!hamClick);
+  };
+  return (
+    <>
+      <div className={css.container}>
+        <div className={css.hamIcon}>
+          <i className={`fas fa-bars fa-2x`} onClick={hamMenuClickHandler}></i>
+        </div>
+        <div className={css.linksContainer}>
+          <Link href="/" className={css.homeLink}>
+            خانه
+          </Link>
+          <Link href="/articles" className={css.links}>
+            مقالات
+          </Link>
+          <Link href="#forum" className={css.links}>
+            فرم ارزیابی
+          </Link>
+          <Link href="#contact" className={css.links}>
+            تماس با ما
+          </Link>
+        </div>
+        <div className={css.logo}>
+          <div className={css.logoImage}>
+            <Image src={logo} alt="Merli-Apply-Logo" />
+          </div>
+          <h3>Merli Apply</h3>
+        </div>
+      </div>
+      {hamClick && (
+        <div className={css.hamMenu}>
+          <Link href="/" className={css.links}>
+            خانه
+          </Link>
+          <Link className={css.links} href="/articles">
+            مقالات
+          </Link>
+          <Link className={css.links} href="#forum">
+            فرم ارزیابی
+          </Link>
+          <Link className={css.links} href="#contact">
+            تماس با ما
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
